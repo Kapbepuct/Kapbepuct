d6306f0f76b4642297e520cad6d3fed3351d3cd0
Kapbepuct <urban2024python@gmail.com>
2024-10-14 23:11:39 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,13 +1,8 @@
-#1st program
-print(9**0.5*5)
-
-#2nd program
-print(9.99 > 9.98 and 1000 != 1000.1)
-
-#3rd program
-print(2*2+2)
-print(2*(2+2))
-print(2*2+2 == 2*(2+2))
-
-#4th program
-print(int(float('123.456')*10)%10)
+name = 'yuriy'
+print('Name:', name)
+age = 50
+print('Age:', age)
+age = age + 1
+print('New_age:', age)
+Is_Student = True
+print(Is_Student)
