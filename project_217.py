f1f564c7519f94c96459e6467e1730851cdaa096
Kapbepuct <urban2024python@gmail.com>
2024-10-14 23:36:01 +0300

    Add files via upload

---
+++ project_217.py
@@ -0,0 +1,5 @@
+hw_quantity = 12
+all = 1.5
+name = 'Python'
+one = all / hw_quantity
+print('Курс:', name,',', 'всего задач:', hw_quantity, ',', 'затрачено часов:', all, ',', 'среднее время выполнения:', one )
